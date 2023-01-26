ca69e91fbb18711352cf642ce805de445beac4e9
menice4 <95630289+menice4@users.noreply.github.com>
2023-01-26 15:28:58 +0000

    fixed fern

--- js/recursion3.js
+++ js/recursion3.js
@@ -1,4 +1,4 @@
-let brown1, brown2
+let brown1, brown2, slider, slider2;
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
@@ -6,22 +6,28 @@ function setup() {
     strokeJoin(ROUND);
     brown1 = color('#d0dea7')
     brown2 = color("#63743e")
-    slider = createSlider(10, 90, 45);
+    slider = createSlider(10, 60, 10);
     slider.position(10, 10)
     slider.style("width", "200px");
     slider.input(draw);
+    slider2 = createSlider(150, 350, 250);
+    slider2.position(10,35)
+    slider2.style("width","200px" )
+    slider2.input(draw);
     noLoop()
 
 }
 function draw() {
     background(0);
     resetMatrix();
+    let initalheight = map(slider.value(), 10, 90 , 50, 550)
+    console.log(slider.value())
 
     translate(width / 2, height);
-    branch(250, 1);
+    branch(initalheight, 1);
     resetMatrix();
     translate(width / 2, height);
-    branch(250,-1)
+    branch(initalheight,-1)
 }
 function branch(len, direction) {// length of branch to draw
     let maxAngle = slider.value()
